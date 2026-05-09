1445fcbf899d80d0aba8b090b605e5fdd2c8e9ea
SharwanKunwar <sharwankunwar07@gmail.com>
2026-05-09 12:45:40 +0545

    added day02 practice questions

--- JAVA/DemoPracticeProgram/Demo/src/main/java/org/example/TestQ02/BankAccount.java
+++ JAVA/DemoPracticeProgram/Demo/src/main/java/org/example/TestQ02/BankAccount.java
@@ -1,9 +1,17 @@
 package org.example.TestQ02;
 
 public class BankAccount {
-    public double balance = 10;
+    private double balance = 0;
 
+    //deposit
     public void deposit(double amount) {
-        balance += amount;
+        if(amount>0){
+            balance += amount;
+        }
+    }
+
+    // check balance
+    public void checkBalance(){
+        System.out.println("Your Total balance is : "+balance);
     }
 }
--- JAVA/DemoPracticeProgram/Demo/src/main/java/org/example/TestQ02/Code.java
+++ JAVA/DemoPracticeProgram/Demo/src/main/java/org/example/TestQ02/Code.java
@@ -2,10 +2,8 @@ package org.example.TestQ02;
 
 public class Code {
     static void main(String[] args) {
-        BankAccount b1 = new BankAccount();
-        b1.balance = -5;
-        b1.deposit(b1.balance);
-        System.out.println(b1.balance);
-        System.out.println("program end");
+        BankAccount acc = new BankAccount();
+        acc.deposit(150);
+        acc.checkBalance();
     }
 }
