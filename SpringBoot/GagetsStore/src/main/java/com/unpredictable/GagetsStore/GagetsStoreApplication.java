3e193cf21150a8c305d1f2360dd6d4be7b1b4486
SharwanKunwar <sharwankunwar07@gmail.com>
2026-05-19 08:32:51 +0545

    basic setup

---
+++ SpringBoot/GagetsStore/src/main/java/com/unpredictable/GagetsStore/GagetsStoreApplication.java
@@ -0,0 +1,13 @@
+package com.unpredictable.GagetsStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GagetsStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GagetsStoreApplication.class, args);
+	}
+
+}
